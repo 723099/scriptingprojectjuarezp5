fd9b34e9792b23bd4acfc931620aaf5016bca1bb
jeremiah <723099@student.dusd.net>
2023-11-30 13:50:44 -0800

    wrote 2 scripts
    2 scripts for scripting project

---
+++ scriptingprojectp5juarez/Assets/Scripts/AnotherClass.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int apples;
+    public int banannas;
+
+
+    private int stapler;
+    private int sellotapes;
+
+
+    // Start is called before the first frame update
+    public void FruitMachine (int a, int b)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
